ddb6400b760392bdc7263abeb5f77a67af4229fa
alexmi94 <alexandrecance94@gmail.com>
2021-08-04 19:54:58 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    let footer_p = document.querySelector("footer p");
+    footer_p.innerHTML += "&copy; " + new Date().getFullYear() + " -  Alexmi94"
+/*
+    if (screen && screen.width < 480) {
+        
+      }
+*/
+});
